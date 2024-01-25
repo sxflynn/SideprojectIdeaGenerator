65e8e41c50411aa3a835485ecc7a6177d30a2c97
Stephen Flynn <stephenflynn@me.com>
2024-01-25 01:11:01 -0500

    refactored main() to better mimic FastAPI main method

--- main_cli.py
+++ main_cli.py
@@ -2,19 +2,19 @@ from src.provider_engine import LLMProvider, Client
 from src.prompt_engine import PromptEngine
 from src.config import Config
 
-
-def run_prompt():
+def run_prompt(unknown_tech, known_tech):
     config = Config("JanAi")
     provider = LLMProvider(config)
     client = Client(provider)
-    unknown_tech = ["Backend", "databases", "Java"]
-    known_tech = ["Javascript", "React", "Typescript", "Vue"]
     generated_prompt = PromptEngine.prompt_project(known_tech,unknown_tech)
     response_content = client.prompt(generated_prompt, full_response=True)
-    print(response_content)
+    return response_content
 
 def main():
-    run_prompt()
+    unknown_tech = ["Backend", "databases", "Java"]
+    known_tech = ["Javascript", "React", "Typescript", "Vue"]
+    response = run_prompt(unknown_tech, known_tech)
+    print (response)
 
 if __name__ == "__main__":
     main()
