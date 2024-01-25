05aa1d5db3a8f9412493b4c058e962626aa703f6
Stephen Flynn <stephenflynn@me.com>
2024-01-25 00:45:59 -0500

    Refactored to allow for dynamic prompt arguments

--- src/provider_engine.py
+++ src/provider_engine.py
@@ -1,27 +1,31 @@
 from openai import OpenAI
+from src.config import Config
 
 class LLMProvider:
-    def __init__(self, config):
+    def __init__(self, config: Config):
         self.url = config.url
         self.key = config.key
-        self.model = config.model
+        self.model = config.selected_model
 
 class Client:
-    def __init__(self, provider):
+    def __init__(self, provider: LLMProvider):
         self.provider = provider
         self.client = OpenAI(
         api_key=provider.key,
         base_url=provider.url
         )
     
-    def prompt(self, prompt_text,full_response=False):
-        response = self.client.chat.completions.create(
-            model=self.provider.model,
-            messages=[
+    def prompt(self, prompt_text: str, full_response=False, json_mode=False):
+        request_args = {
+            "model": self.provider.model,
+            "messages": [
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": prompt_text}
             ]
-        )
+        }
+        if json_mode:
+            request_args["response_format"] = {"type": "json_object"}
+        response = self.client.chat.completions.create(**request_args)
         if full_response:
             return response
         else:
