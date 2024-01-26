2e24e6d01890cd65fef769222a17feab0c00d3d7
Stephen Flynn <stephenflynn@me.com>
2024-01-25 23:27:51 -0500

    refactoring prompt engine and validator

--- main_cli.py
+++ main_cli.py
@@ -1,20 +1,34 @@
+import json
+from pydantic import BaseModel
+from typing import List
 from src.provider_engine import LLMProvider, Client
 from src.prompt_engine import PromptEngine
 from src.config import Config
+from src.response_validator import ProjectResponse, validate_response
 
-def run_prompt(unknown_tech, known_tech):
-    config = Config("JanAi")
+class TechList(BaseModel):
+    unknown_tech: List[str]
+    known_tech: List[str]
+    topics: List[str]
+    
+def run_prompt(user_input:TechList) -> ProjectResponse:
+    config = Config("OpenAI")
     provider = LLMProvider(config)
     client = Client(provider)
-    generated_prompt = PromptEngine.prompt_project(known_tech,unknown_tech)
-    response_content = client.prompt(generated_prompt, full_response=True)
+    prompt_engine = PromptEngine(user_input)
+    system_message = prompt_engine.create_system_message()
+    user_prompt = prompt_engine.create_prompt()
+    response_content = client.prompt(user_prompt, system_message, full_response=False, json_mode=True)
     return response_content
 
 def main():
-    unknown_tech = ["Backend", "databases", "Java"]
-    known_tech = ["Javascript", "React", "Typescript", "Vue"]
-    response = run_prompt(unknown_tech, known_tech)
-    print (response)
+    user_input = TechList(
+        unknown_tech= ["Backend", "databases", "Java"],
+        known_tech= ["Javascript", "React", "Typescript", "Vue"],
+        topics= ["Dancing", "Cooking"]
+    )
+    response = run_prompt(user_input)
+    print(validate_response(response))
 
 if __name__ == "__main__":
     main()
