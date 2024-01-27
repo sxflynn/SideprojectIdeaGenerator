6479215ffff28c273f735d42f71273664f1156d0
Stephen Flynn <stephenflynn@me.com>
2024-01-26 23:27:05 -0500

    added Pydantic validation on LLM output

--- main_cli.py
+++ main_cli.py
@@ -1,8 +1,10 @@
 from typing import List
+from devtools import pprint
 from src.provider_engine import LLMProvider, Client
 from src.prompt_engine import PromptEngine
 from src.config import Config
 from src.response_validator import ProjectResponse, TechList, validate_response
+
     
 # simulating incoming POST request
 post_request = {
@@ -13,19 +15,20 @@ post_request = {
 
 #future FastAPI POST endpoint /prompt
 def run_prompt(user_input:TechList) -> ProjectResponse:
-    config = Config("OpenAI")
+    config = Config("JanAi")
     provider = LLMProvider(config)
     client = Client(provider)
     prompt_engine = PromptEngine(user_input)
-    system_message = prompt_engine.create_system_message()
+    system_message = prompt_engine.create_system_message(json=False)
     user_prompt = prompt_engine.create_prompt()
-    response_content = client.prompt(user_prompt, system_message, full_response=False, json_mode=True)
-    return response_content
+    response_content = client.prompt(user_prompt, system_message, full_response=False, json_mode=False)
+    parsed_response:ProjectResponse = validate_response(response_content)
+    return parsed_response
 
 def main():
     user_input = TechList(**post_request)
     response = run_prompt(user_input)
-    print(validate_response(response))
+    pprint (response)
 
 if __name__ == "__main__":
     main()
