a78ef743601d02e6c1974def62f8056d9ef7f430
Stephen Flynn <stephenflynn@me.com>
2024-02-10 01:39:17 -0500

    trying multiselect

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,24 +1,41 @@
-import { Box, Button, TextInput } from "@mantine/core";
+import { Box, Button, MultiSelect } from "@mantine/core";
 import { useForm } from "@mantine/form";
 
 export function Form() {
   const form = useForm({
     initialValues: {
-      knownTech: "",
-      unknownTech: "",
-      topics: "",
+      knownTech: [],
+      unknownTech: [],
+      topics: [],
     },
   });
 
   return (
     <Box>
       <form onSubmit={form.onSubmit((values) => console.log(values))}>
-        <TextInput label="Tech you know" {...form.getInputProps("knownTech")} />
-        <TextInput label="Tech you don't know" {...form.getInputProps("unknownTech")} />
-        <TextInput label="Topics you like" {...form.getInputProps("topics")} />
+        {/* <TextInput label="Tech you know" {...form.getInputProps("knownTech")} /> */}
+        <MultiSelect
+      label="Tech you know"
+      data={['React', 'Angular', 'Vue', 'Svelte']}
+      searchable
+      {...form.getInputProps("knownTech")}
+    />
+        <MultiSelect
+      label="Tech you don't know"
+      data={['React', 'Angular', 'Vue', 'Svelte']}
+      searchable
+      {...form.getInputProps("unknownTech")}
+    />
+        <MultiSelect
+      label="Topics you like"
+      data={['React', 'Angular', 'Vue', 'Svelte']}
+      searchable
+      {...form.getInputProps("topics")}
+    />
       <Button type="submit">Submit</Button>
       
-      
+  
+
       </form>
     </Box>
   );
