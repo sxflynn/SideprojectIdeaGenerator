a8c9bd83cd91acd28be14f4a94d344340ab2925a
Stephen Flynn <stephenflynn@me.com>
2024-02-10 01:38:49 -0500

    header design

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,16 @@
-import {Text} from '@mantine/core';
+import {Container, Text} from '@mantine/core';
+import classes from './Header.module.css'
+
 
 export function Header() {
 
     return (
-        <Text>Side Project Idea Generator</Text>
+        <header className={classes.header}>
+           <Container size="md"> 
+           <Text>Side Project Idea Generator</Text>
+           </Container>
+        </header>
+        
 
     )
 }
