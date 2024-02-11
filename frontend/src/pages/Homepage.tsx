311acea471bf88257f152846410655af7223c7e0
Stephen Flynn <stephenflynn@me.com>
2024-02-11 00:16:40 -0500

    form response state lifted to parent component

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import {Text} from '@mantine/core';
 import { Form } from '../components/Form'
+import { ProjectDisplay, ProjectResponse } from '../components/ProjectDisplay';
 
 export function Homepage() {
-
+    const [projectData, setProjectData] = useState<ProjectResponse | null>(null);
+    
     return (
         <>
         <Text>Use this form below</Text>
-        <Form/>
+        <Form onFormSubmit={setProjectData} />
+        {projectData && <ProjectDisplay data={projectData} />}
         </>
     )
 }
