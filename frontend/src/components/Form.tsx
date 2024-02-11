841ec79d10d12ff746a13b9c26baeed6d015cfe6
Stephen Flynn <stephenflynn@me.com>
2024-02-11 00:15:51 -0500

    form is functional, needs more to choose from list

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,30 +1,66 @@
 import { Box, Button, MultiSelect } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { ProjectResponse } from "./ProjectDisplay";
 
-export function Form() {
-  const form = useForm({
+type TechList = {
+  unknown_tech: string[];
+  known_tech: string[];
+  topics: string[];
+}
+
+type FormProps = {
+  onFormSubmit: (data: ProjectResponse) => void;
+}
+
+
+export function Form({ onFormSubmit }: FormProps) {
+    
+  const form = useForm<TechList>({
     initialValues: {
-      knownTech: [],
-      unknownTech: [],
+      unknown_tech: [],
+      known_tech: [],
       topics: [],
     },
   });
 
+  const handleSubmit = async (values: TechList) => {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/prompt', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data: ProjectResponse = await response.json();
+      onFormSubmit(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  
+
+
   return (
     <Box>
-      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+      <form onSubmit={form.onSubmit(handleSubmit)}>
         {/* <TextInput label="Tech you know" {...form.getInputProps("knownTech")} /> */}
         <MultiSelect
       label="Tech you know"
       data={['React', 'Angular', 'Vue', 'Svelte']}
       searchable
-      {...form.getInputProps("knownTech")}
+      {...form.getInputProps("known_tech")}
     />
         <MultiSelect
       label="Tech you don't know"
       data={['React', 'Angular', 'Vue', 'Svelte']}
       searchable
-      {...form.getInputProps("unknownTech")}
+      {...form.getInputProps("unknown_tech")}
     />
         <MultiSelect
       label="Topics you like"
