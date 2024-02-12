2d4e0d3c9e93cc5751c7b5d1b427527573ba96cc
Stephen X. Flynn <stephenflynn@me.com>
2024-02-12 12:43:39 -0500

    added homepage helper text

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,3 +1,4 @@
+import { Text } from '@mantine/core'
 import { useState } from 'react';
 import { Form } from '../components/Form'
 import { ProjectDisplay, ProjectResponse } from '../components/ProjectDisplay';
@@ -7,6 +8,7 @@ export function Homepage() {
     
     return (
         <>
+        <Text pb="lg">Use this tool to generate a full stack side project based on your skills and interests.</Text>
         <Form onFormSubmit={setProjectData} />
         {projectData && <ProjectDisplay data={projectData} />}
         </>
