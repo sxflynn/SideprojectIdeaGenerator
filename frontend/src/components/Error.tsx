99e3d5d98605cd960a39ba087fdfb93d12737319
Stephen X. Flynn <stephenflynn@me.com>
2024-02-12 12:42:53 -0500

    First commit

---
+++ frontend/src/components/Error.tsx
@@ -0,0 +1,18 @@
+type ErrorProps = {
+    error: string;
+    onDismiss: () => void;
+}
+
+export function Error({error,onDismiss}: ErrorProps){
+
+    return (
+        <div>
+            {error && (
+                <div className="error-message">
+                    {error}
+                    <button onClick={onDismiss} className="dismiss-error-btn">X</button>
+                </div>
+            )}
+        </div>
+    );
+    }
