a1466b8a5d20cbab2116fa6e571711ac277d8b47
Stephen Flynn <stephenflynn@me.com>
2024-02-13 21:56:37 -0500

    vite env config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envDir: "../",
   plugins: [react()],
   server: {
-   watch: {
-    usePolling: true,
-   },
-   host: true, // Here
-   strictPort: true,
-   port: 5173, 
- }})
+    watch: {
+      usePolling: true,
+    },
+    host: true, // Here
+    strictPort: true,
+    port: 5173,
+  },
+});
